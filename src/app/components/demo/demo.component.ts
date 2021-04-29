2c9411ce054b93cb3f3e89b6c506093d29eaf4dd
iOznny <f000170014@gmail.com>
2021-04-29 11:58:02 -0500

    c

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html'
 })
 
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string;
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    
   }
 
 }
