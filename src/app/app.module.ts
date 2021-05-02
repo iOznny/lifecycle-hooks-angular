b619fd1e583835891193a4abedf5dbf86fd9ecee
iOznny <f000170014@gmail.com>
2021-05-02 18:04:30 -0500

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PageOneComponent } from './pages/page-one/page-one.component';
@@ -12,7 +13,8 @@ import { DemoComponent } from './components/demo/demo.component';
     DemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/page-one/page-one.component.ts
+++ src/app/pages/page-one/page-one.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-one',
@@ -8,40 +9,52 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class PageOneComponent implements OnInit, OnChanges, DoCheck, 
     AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
+  public seconds: number = 0;
+  public nombre: string = 'Fernando';
+  public timerSubscription!: Subscription;
+
   constructor() { 
     console.log('Constructor PageOne');    
   }
 
   ngOnInit(): void {
     console.log('NgOnInit');
+
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.seconds = i;
+    })
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('Method ngOnChanges ');
+    console.log('Method ngOnChanges');
   }
 
   ngDoCheck(): void {
-    console.log('Method ngDoCheck ');
+    console.log('Method ngDoCheck');
   }
 
   ngAfterContentInit(): void {
-    console.log('Method ngAfterContentInit ');
+    console.log('Method ngAfterContentInit');
   }
 
   ngAfterContentChecked(): void {
-    console.log('Method ngAfterContentChecked ');
+    console.log('Method ngAfterContentChecked');
   }
 
   ngAfterViewInit(): void {
-    console.log('Method ngAfterViewInit ');
+    console.log('Method ngAfterViewInit');
   }
 
   ngAfterViewChecked(): void {
-    console.log('Method ngAfterViewChecked ');
+    console.log('Method ngAfterViewChecked');
   }
 
   ngOnDestroy(): void {
-    console.log('Method ngOnDestroy ');
+    console.log('Method ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+  }
+
+  save() {
   }
 
 }
